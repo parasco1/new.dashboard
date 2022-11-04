411449a335ae8364725b90f4e54536594754da3d
parasco1 <73285583+parasco1@users.noreply.github.com>
2022-11-05 00:34:13 +0100

    Update config.mainnet.ts

--- src/config.mainnet.ts
+++ src/config.mainnet.ts
@@ -55,7 +55,7 @@ export const network: NetworkType = {
   gatewayAddress: 'https://gateway.elrond.com',
   explorerAddress: 'https://explorer.elrond.com',
   delegationContract:
-    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqx8llllsxavffq'
+    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqq50lllls8dl5vl'
 };
 
 export const delegationContractData: Array<DelegationContractType> = [
